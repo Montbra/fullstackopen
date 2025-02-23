4e1cce83792009a5ac4734b7589f708a398dcb05
Montbra <montbra@gmail.com>
2025-02-23 01:08:28 -0300

    Refactor Total component to use arrow function syntax

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -20,7 +20,7 @@ const Content = ({ parts }) => {
   );
 };
 
-function Total({ parts }) {
+const Total = ({ parts }) => {
   const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <div>
